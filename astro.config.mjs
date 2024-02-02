172632b4892761681409f8d3e96094d16f924d5f
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-02-02 08:57:37 +0530

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import compress from "astro-compress";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), compress()],
+  prefetch: {
+    defaultStrategy: "viewport",
+  },
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
@@ -6,6 +7,8 @@ export default {
       lato: "Lato, sans-serif",
       Ephesis: "cursive",
       Hedvig: "Hedvig Letters Serif, serif",
+      tenor: "Tenor Sans, sans-serif",
+      noto: "Noto Serif Display, serif",
     },
     extend: {},
   },
